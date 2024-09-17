f5e2a35fcf266d414fe09f28f2a681e6d91bcae7
Tyuiu.KulkoDA@outlook.com <Tyuiu.KulkoDA@outlook.com>
2024-09-17 19:05:38 +0500

    Task1

---
+++ Tyuiu.KulkoDA.Sprint1.Task1.V28.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.KulkoDA.Sprint1.Task1.V28.Lib;
+
+namespace Tyuiu.KulkoDA.Sprint1.Task1.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            var res  = ds.Calculate(x);
+            Assert.AreEqual(1, res);
+        }
+    }
+}
---
+++ Tyuiu.KulkoDA.Sprint1.Task1.V28/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.KulkoDA.Sprint1.Task1.V28.Lib;
+
+namespace Tyuiu.KulkoDA.Sprint1.Task1.V28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Кулько Д. А. | ИИПб-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема:Базовые навыки работы в C#                                         *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнила: Кулько Дарья Андреевна | ИИПб-24-2                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле (x * 2)/(1 + x)                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("***************************************************************************");
+            double x;
+            Console.WriteLine("Введите значение X:");
+            x= Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(x));
+            Console.ReadLine();
+        }
+    }
+}
