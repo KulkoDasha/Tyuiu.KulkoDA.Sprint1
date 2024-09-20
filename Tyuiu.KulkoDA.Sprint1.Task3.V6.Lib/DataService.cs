6661936019d2da162a1562dfce69aa04905ca13c
Tyuiu.KulkoDA@outlook.com <Tyuiu.KulkoDA@outlook.com>
2024-09-20 20:37:54 +0500

    Task3

---
+++ Tyuiu.KulkoDA.Sprint1.Task3.V6.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿
+using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.KulkoDA.Sprint1.Task3.V6.Lib
+{
+    public class DataService : ISprint1Task3V6
+    {
+        public double TravelCost(double distance, double gasFlow, double gasPrice)
+        {
+            double x = (gasFlow * distance) / 100;
+            double y = x * gasPrice * 2;
+            return Math.Round(y,2);
+        }
+    }
+}
---
+++ Tyuiu.KulkoDA.Sprint1.Task3.V6/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.KulkoDA.Sprint1.Task3.V6.Lib;
+
+namespace Tyuiu.KulkoDA.Sprint1.Task3.V6
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнила: Кулько Д. А. | ИИПб-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнила: Кулько Дарья Андреевна | ИИПб-24-2                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу вычисления стоимости поездки на автомобиле на дачу   *");
+            Console.WriteLine("* (туда и обратно).                                                       *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите расстояние до дачи:");
+            double p = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите расход бензина:");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите цену за литр бензина:");
+            double l = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Поездка на дачу и обратно обойдется в " + ds.TravelCost(p,b,l));
+            Console.ReadKey();
+        }
+    }
+}
