69cc8ca48f4ea88c0ef72a908c85db8e60c713bf
Tyuiu.KulkoDA@outlook.com <Tyuiu.KulkoDA@outlook.com>
2024-09-27 12:16:06 +0500

    Task7.1

--- Tyuiu.KulkoDA.Sprint1.Task7.V4.Lib/DataService.cs
+++ Tyuiu.KulkoDA.Sprint1.Task7.V4.Lib/DataService.cs
@@ -9,7 +9,7 @@ namespace Tyuiu.KulkoDA.Sprint1.Task7.V4.Lib
             double one = x + (x * x) / 4;
             double two = x - y / one;
             double three = Math.Abs((y - Math.Sqrt(Math.Abs(x))) * two);
-            return Math.Round(Math.Log(three, Math.E),2);
+            return Math.Round(Math.Log(three, Math.E),3);
         }
     }
 }
